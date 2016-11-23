89978e60ebf18b11bb76c15b6a674d79b4d42af8
eteubert <eric@ericteubert.de>
2016-11-23 13:54:55 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var githubConfig = require('github-config');
+var dist = require('br-wordpress-gulp-dist');
+var path = require('path');
+
+var config = {
+    themeFile:  path.resolve('./style.css'),
+    manifest:   path.resolve('./package.json'),
+    token:      githubConfig().token
+};
+
+gulp.task('release', function() {
+    runSequence(
+        'bump-version',
+        'update-theme-file-version',
+        'commit-changes',
+        'tag-and-push',
+        // add css/js processing here if necessary
+        'make-asset',
+        'deploy-asset'
+    );
+});
+
+gulp.task('bump-version', dist.bumpVersion);
+gulp.task('update-theme-file-version', function() {
+    return dist.updateWordPressThemeFile(config);
+});
+gulp.task('make-asset', dist.makeReleaseAsset);
+gulp.task('deploy-asset', function() {
+    return dist.deployReleaseAsset(config);
+});
+gulp.task('commit-changes', dist.commitAllChanges);
+gulp.task('tag-and-push', dist.tagAndPush);
